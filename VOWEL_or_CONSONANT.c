c027226f4825f61dbf5ece93b9ba392ed1ad759c
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-05-26 12:34:25 +0530

    #include<stdio.h>  int main() {     char cletter;     scanf("%c",&cletter);     if(cletter=='a'||cletter=='e'||cletter=='i'||cletter=='o'||cletter=='u'||cletter=='A'||cletter=='E'||cletter=='I'||cletter=='O'||cletter=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{
+    char cletter;
+    scanf("%c",&cletter);
+    if(cletter=='a'||cletter=='e'||cletter=='i'||cletter=='o'||cletter=='u'||cletter=='A'||cletter=='E'||cletter=='I'||cletter=='O'||cletter=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
